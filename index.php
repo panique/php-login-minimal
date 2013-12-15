24c1bda6ead3e8179adf5c8246195cab33780264
Panique <panique@web.de>
2013-12-15 08:36:22 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * A simple, clean and secure PHP Login Script
+ *
+ * MINIMAL VERSION
+ * (check the website / GitHub for other versions)
+ *
+ * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
+ *
+ * Please note: this is just the minimal version of the login script, so if you need a more
+ * advanced version, have a look on http://php-login.net/ or https://github.com/panique/php-login.
+ * There is also a professional framework-like version with much more features.
+ *
+ * @author Panique
+ * @link https://github.com/panique/php-login-minimal/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// checking for minimum PHP version
+if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+    require_once("libraries/password_compatibility_library.php");
+}
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+    include("views/logged_in.php");
+
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("views/not_logged_in.php");
+}
---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * A simple, clean and secure PHP Login Script
+ *
+ * MINIMAL VERSION
+ * (check the website / GitHub for other versions)
+ *
+ * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
+ *
+ * Please note: this is just the minimal version of the login script, so if you need a more
+ * advanced version, have a look on http://php-login.net/ or https://github.com/panique/php-login.
+ * There is also a professional framework-like version with much more features.
+ *
+ * @author Panique
+ * @link https://github.com/panique/php-login-minimal/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// checking for minimum PHP version
+if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
+    // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+    require_once("libraries/password_compatibility_library.php");
+}
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// load the registration class
+require_once("classes/Registration.php");
+
+// create the registration object. when this object is created, it will do all registration stuff automaticly
+// so this single line handles the entire registration process.
+$registration = new Registration();
+
+// showing the register view (with the registration form, and messages/errors)
+include("views/register.php");
