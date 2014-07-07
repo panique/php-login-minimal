6aeffa086c8e71d1d39f7af4a0f1a4c953bff45d
Omar Malave <omalave@gmail.com>
2014-07-07 18:19:31 -0500

    Update Login.php
    This is a improvement for a pdo connection

--- classes/Login.php
+++ classes/Login.php
@@ -41,9 +41,10 @@ class Login
 
     /**
      * log in with post data
+     * just include your database class for $conn
      */
-    private function dologinWithPostData()
-    {
+    private function dologinWithPostData() {
+        
         // check login form contents
         if (empty($_POST['user_name'])) {
             $this->errors[] = "Username field was empty.";
@@ -51,54 +52,59 @@ class Login
             $this->errors[] = "Password field was empty.";
         } elseif (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
 
-            // create a database connection, using the constants from config/db.php (which we loaded in index.php)
-            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-            // change character set to utf8 and check it
-            if (!$this->db_connection->set_charset("utf8")) {
-                $this->errors[] = $this->db_connection->error;
-            }
+                $db   = new database();
+                $conn = $db->conn();
 
-            // if no connection errors (= working database connection)
-            if (!$this->db_connection->connect_errno) {
+                try {
 
                 // escape the POST stuff
-                $user_name = $this->db_connection->real_escape_string($_POST['user_name']);
+                $user_name = $_POST['user_name'];
 
-                // database query, getting all the info of the selected user (allows login via email address in the
-                // username field)
                 $sql = "SELECT user_name, user_email, user_password_hash
                         FROM users
-                        WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
-                $result_of_login_check = $this->db_connection->query($sql);
+                        WHERE user_name = :user_name OR user_email = :user_email;";
 
-                // if this user exists
-                if ($result_of_login_check->num_rows == 1) {
+   
+                $query = $conn->prepare( $sql );
+                                /*** bind the paramaters ***/
+                   $query->bindParam(':user_name', $user_name, PDO::PARAM_STR);
+                   $query->bindParam(':user_email', $user_name, PDO::PARAM_STR);
 
-                    // get result row (as an object)
-                    $result_row = $result_of_login_check->fetch_object();
+                $query->execute();
+                $row = $query->fetch(PDO::FETCH_ASSOC);
 
-                    // using PHP 5.5's password_verify() function to check if the provided password fits
-                    // the hash of that user's password
-                    if (password_verify($_POST['user_password'], $result_row->user_password_hash)) {
+                        // if this user exists
+                        if ($row) {
 
-                        // write user data into PHP SESSION (a file on your server)
-                        $_SESSION['user_name'] = $result_row->user_name;
-                        $_SESSION['user_email'] = $result_row->user_email;
-                        $_SESSION['user_login_status'] = 1;
+                            // using PHP 5.5's password_verify() function to check if the provided password fits
+                            // the hash of that user's password
+                            if (password_verify($_POST['user_password'], $row["user_password_hash"])) {
 
-                    } else {
-                        $this->errors[] = "Wrong password. Try again.";
-                    }
-                } else {
-                    $this->errors[] = "This user does not exist.";
+                                // write user data into PHP SESSION (a file on your server)
+                                $_SESSION['user_name'] = $result_row->user_name;
+                                $_SESSION['user_email'] = $result_row->user_email;
+                                $_SESSION['user_login_status'] = 1;
+
+                            } else {
+                                $this->errors[] = "Wrong password. Try again.";
+                            }
+                        } else {
+                            $this->errors[] = "This user does not exist.";
+                        }
+
+
+                } catch ( PDOException $e ) {
+
+                    var_dump($e->getMessage());
+                    echo 'Caught exception: ', $e->getMessage(), "\n";
+
+                    return false;  
+
                 }
-            } else {
-                $this->errors[] = "Database connection problem.";
-            }
+
         }
     }
-
     /**
      * perform the logout
      */
