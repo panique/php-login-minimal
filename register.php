326be96d8ed60eebaa4a6a68904b04c8e87a2acf
Chris <panique@users.noreply.github.com>
2016-08-10 17:07:39 +0200

    removed outdated link

--- register.php
+++ register.php
@@ -2,7 +2,6 @@
 
 /**
  * A simple, clean and secure PHP Login Script / MINIMAL VERSION
- * For more versions (one-file, advanced, framework-like) visit http://www.php-login.net
  *
  * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
  *
