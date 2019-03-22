a0411a75a3c2a83809bc9a1bce7b4127dd516a4c
Benjamin Van Renterghem <42121860+refortunacc@users.noreply.github.com>
2019-03-22 12:28:25 +0100

    Update csrf.php

--- views/csrf.php
+++ views/csrf.php
@@ -1,17 +1,24 @@
-<?php
-
-if(empty($SESSION['key'])){
-  $_SESSION['key'] = bin2hex(random_bytes(32));
+function generateRandomString($length = 32) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
 
-$csrf = hash_hmac('sha256', 'index.php', $_SESSION['key']);
+if(empty($SESSION['key'])){
+  $_SESSION['key'] = bin2hex(generateRandomString());
+}
 
-if (isset($_POST)) {
+$csrf = hash_hmac('sha256', 'header.php', $_SESSION['key']);
+
+if (!empty(isset($_POST))) {
   if (hash_equals($csrf, $_POST['csrf'])) {
 
   } else {
     die('Invalid form submission. Error 44');
+    header('Location: /');
   }
 }
-
- ?>
